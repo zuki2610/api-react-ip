ad9cd0e449934c043aa6c67695f1fd4093fd75a3
Isabel Palacios <issabelpalacios@gmail.com>
2023-02-02 12:28:50 -0300

    FIX: custom

--- src/Components/MiApi.jsx
+++ src/Components/MiApi.jsx
@@ -4,12 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Footer from './Footer';
 import {Form, FormControl, Row, Col,Card, Image } from "react-bootstrap";
+import Menu from './Menu';
 const MiApi = () => {
   const [superHeroes, setSuperHeroes] = useState([]);
 
   const obtenerDatos = async (parametro="") => {
     try {
-      const data = await fetch("/db/marvel.json");     
+      const data = await fetch("https://raw.githubusercontent.com/zuki2610/api-react-ip/c86d072c9eabc9b5a2b13a117ab5e25cdf0bc672/public/db/marvel.json");     
       let superHeroes = await data.json();
       if (parametro !== "") { 
         superHeroes = superHeroes.filter(superhero =>
@@ -31,8 +32,11 @@ const MiApi = () => {
   return (
     <>
         <div className="Container bg-dark m-0">
+        <Menu isLogin={false}/>
         <h1 className="text-bg-dark ps-5 pt-5 m-0 title">LOS MEJORES SUPERHEROES DE LA HISTORIA ESTÁN AQUÍ</h1>
-        <Image className="imagen" src="https://raw.githubusercontent.com/zuki2610/renderizacion_react/main/public/assets/img/Logo1.png"/>
+        <div className=" d-flex justify-content-center mt-5 mb-5">
+    <Image  src="https://raw.githubusercontent.com/zuki2610/renderizacion_react/main/public/assets/img/Logo1.png"/>
+    </div>
         <Form inline className="mb-5 pe-5 me-5 d-flex justify-content-end vibrate-1">
         <FormControl className="color-change-5x" style={{ width: '12rem', height: '5vh'}}
           type="text"
@@ -40,6 +44,7 @@ const MiApi = () => {
           onChange={handleSearch}
         />
       </Form>
+      <div className= "d-flex justify-content-center mt-5 mb-5">
         <Row className="m-0 row">
       {superHeroes.map(superhero => (
         <Col key={superhero.id} xs={12} md={6} lg={4}>
@@ -53,6 +58,7 @@ const MiApi = () => {
         </Col>
       ))}
     </Row>
+    </div>
         <Footer/>
         </div>
     </>
