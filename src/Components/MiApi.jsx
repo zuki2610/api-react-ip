1beccc68fc13a6397d9fc9a3625535d06dee376d
Isabel Palacios <issabelpalacios@gmail.com>
2023-02-02 02:45:37 -0300

    ADD STYLES

--- src/Components/MiApi.jsx
+++ src/Components/MiApi.jsx
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from "react";
 import '../index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import Header from './Header';
-import Menu from './Menu';
 import Footer from './Footer';
-import {Form, FormControl, Container, Row, Col,Card, Image } from "react-bootstrap";
+import {Form, FormControl, Row, Col,Card, Image } from "react-bootstrap";
 const MiApi = () => {
   const [superHeroes, setSuperHeroes] = useState([]);
 
@@ -32,22 +30,22 @@ const MiApi = () => {
   }, []);
   return (
     <>
-      <Container>
-        <Menu/>
-        <Header titulo="Personajes de Marvel y Comics"/>
-        <Form inline className="mb-3">
-        <FormControl
+        <div className="Container bg-dark m-0">
+        <h1 className="text-bg-dark ps-5 pt-5 m-0 title">LOS MEJORES SUPERHEROES DE LA HISTORIA ESTÁN AQUÍ</h1>
+        <Image className="imagen" src="https://raw.githubusercontent.com/zuki2610/renderizacion_react/main/public/assets/img/Logo1.png"/>
+        <Form inline className="mb-5 pe-5 me-5 d-flex justify-content-end vibrate-1">
+        <FormControl className="color-change-5x" style={{ width: '12rem', height: '5vh'}}
           type="text"
           placeholder="Buscar superhéroe"
           onChange={handleSearch}
         />
       </Form>
-        <Row>
+        <Row className="m-0 row">
       {superHeroes.map(superhero => (
         <Col key={superhero.id} xs={12} md={6} lg={4}>
-          <Card>
-            <Card.Body>
-              <Image src={superhero.imagen} alt={superhero.name} fluid />
+          <Card  style={{ width: '18rem', height: '54vh' }} className="mb-5 ms-5 flip-scale-up-ver">
+            <Card.Body className="bg-secondary text-light">
+              <Image src={superhero.imagen} alt={superhero.name} variant="top" className="rounded mb-4 p-0" fluid style={{ width: '18rem', height: '34vh' }}/>
               <Card.Title>{superhero.name}</Card.Title>
               <Card.Text>{superhero.comic}</Card.Text>
             </Card.Body>
@@ -56,7 +54,7 @@ const MiApi = () => {
       ))}
     </Row>
         <Footer/>
-      </Container>
+        </div>
     </>
   );
 };
